ab2fe9288b4923de01dfbc565dab1216c331bd18
Rohit kumar <sahrohitkumar10@gmail.com>
2025-03-31 12:03:01 +0530

    Update PollHistory.jsx
    fix bugs

--- src/Pages/poll-history/PollHistory.jsx
+++ src/Pages/poll-history/PollHistory.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import backIcon from "../../assets/back.svg";
-import "./PollHistoryPage.css"; 
+import "./PollHistoryPage.css"; // Import CSS for custom styling
 
 let apiUrl =
   import.meta.env.VITE_NODE_ENV === "production"
@@ -73,47 +73,64 @@ const PollHistoryPage = () => {
           );
 
           return (
-            <div key={poll._id} className="card mb-4">
-              <div className="pb-3">{`Question ${++questionCount}`}</div>
-              <div className="card-body">
-                <h6 className="question py-2 ps-2 text-left rounded text-white">
-                  {poll.question} ?
-                </h6>
-                <div className="list-group mt-4">
-                  {poll.options.map((option) => (
-                    <div
-                      key={option._id}
-                      className="list-group-item rounded m-2"
-                    >
-                      <div className="d-flex justify-content-between align-items-center">
-                        <span>{option.text}</span>
-                        <span>
-                          {Math.round(
-                            calculatePercentage(option.votes, totalVotes)
-                          )}
-                          %
-                        </span>
+            <>
+              <div className="pb-3">{`Quetion ${++questionCount}`}</div>
+              <div key={poll._id} className="card mb-4">
+                <div className="card-body">
+                  <h6 className="question py-2 ps-2 text-left rounded text-white">
+                    {poll.question} ?
+                  </h6>
+                  <div className="list-group mt-4">
+                    {poll.options.map((option) => (
+                      <div
+                        key={option._id}
+                        className="list-group-item rounded m-2"
+                      >
+                        <div className="d-flex justify-content-between align-items-center">
+                          <span>{option.text}</span>
+                          <span>
+                            {Math.round(
+                              calculatePercentage(option.votes, totalVotes)
+                            )}
+                            %
+                          </span>
+                        </div>
+                        <div className="progress mt-2">
+                          <div
+                            className="progress-bar progress-bar-bg"
+                            role="progressbar"
+                            style={{
+                              width: `${calculatePercentage(
+                                option.votes,
+                                totalVotes
+                              )}%`,
+                            }}
+                            aria-valuenow={option.votes}
+                            aria-valuemin="0"
+                            aria-valuemax="100"
+                          ></div>
+                        </div>
                       </div>
-                      <div className="progress mt-2">
-                        <div
-                          className="progress-bar progress-bar-bg"
-                          role="progressbar"
-                          style={{
-                            width: `${calculatePercentage(
-                              option.votes,
-                              totalVotes
-                            )}%`,
-                          }}
-                          aria-valuenow={option.votes}
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 </div>
               </div>
-            </div>
+            </>
           );
         })
       ) : null}
